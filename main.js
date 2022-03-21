e7e07a10527662feee255d20f965e645b87b3316
Gianluca Uva <gianluca360hc@gmail.com>
2022-03-21 19:20:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,20 +10,8 @@
     let searchAddress = document.getElementById('inputAddress').value;
     const options = { chain: 'bsc', address: searchAddress };
     const BSCNFTs = await Moralis.Web3API.account.getNFTs(options);
-    //console.log(BSCNFTs);
-    BSCNFTs.array.forEach(element => {
-        
-    });(function(nft){
-        let url = nft.token_uri;
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            $("#content").html($("#content").html()+"<h2>"+data.name+"</h2>");
-            $("#content").html($("#content").html()+"<h3>"+data.description+"</h3>");
-            $("#content").html($("#content").html()+"<video type='video/mp4' width='320' height='240' src='+data.animation_url+'/>");
-            
-        })
-    })
+    console.log(BSCNFTs);
+   
    }
 
    
